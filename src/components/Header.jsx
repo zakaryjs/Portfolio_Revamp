6542be7a1a53388e25c7b59effd0a0315f9d0bf9
Zakary <119669322+zakaryjs@users.noreply.github.com>
2023-11-01 22:52:06 +0800

    feat: added fade transition to landing page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,33 @@
 import '../styles/Header.css'
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { useSpring, animated } from '@react-spring/web'
 
 
 export default function Header() {
 
+    const mainFade = useSpring({
+        from: { opacity: 0 },
+        opacity: 1,
+        config: { duration: 2500 }
+    })
+    
+    const subFade = useSpring({
+        from: { opacity: 0 },
+        opacity: 1,
+        config: { duration: 1000 },
+        delay: 2500
+    })    
 
     return (
         <>
         <div id='mainHeaderText'>
-        <h1 className='mainHeader'>Zakary Sutherland</h1>
-        <h3 className='mainHeader'>Web Developer in Development</h3>
-        <div>
+        <animated.div style={mainFade}>
+            <h1 className='mainHeader'>Zakary Sutherland</h1>
+            <h3 className='mainHeader'>Web Developer in Development</h3>
+        </animated.div>
+        <animated.div style={subFade}>
             <FaGithub className='socialIcons' /> <FaLinkedin className='socialIcons' />
-        </div>
+        </animated.div>
         </div>
         </>
     )
