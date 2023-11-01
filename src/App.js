aa7c47a8a5428476e0d9a5a4544fa0c6200c8a67
Zakary <119669322+zakaryjs@users.noreply.github.com>
2023-11-01 16:00:27 +0800

    feat: began work on landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id='mainHeaderText'>
+      <h1 className='mainHeader'>Zakary Sutherland</h1>
+      <h3 className='mainHeader'>Web Developer in Development</h3>
+      </div>
     </div>
   );
 }
