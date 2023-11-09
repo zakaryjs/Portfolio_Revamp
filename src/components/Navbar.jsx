0d9506cd0144453ce114fd061d1c368f0d23b6ed
Zakary <119669322+zakaryjs@users.noreply.github.com>
2023-11-09 19:13:17 +0800

    feat: updated navbar styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,13 @@ import '../styles/Navbar.css'
 
 export default function Navbar(){
 
+    let link = {
+        textDecorationColor: "white",
+        color: "white"
+    }
+
     let activeLink = {
+        color: "yellow",
         textDecorationColor: "yellow"
     }
 
@@ -11,27 +17,27 @@ export default function Navbar(){
         <nav>
             <ul>
                 <li>
-                    <NavLink to="/" style={({isActive}) => isActive ? activeLink : undefined}>
+                    <NavLink to="/" style={({isActive}) => isActive ? activeLink : link}>
                         Home
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to="/about" style={({isActive}) => isActive ? activeLink : undefined}>
+                    <NavLink to="/about" style={({isActive}) => isActive ? activeLink : link}>
                         About Me
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to="/skills" style={({isActive}) => isActive ? activeLink : undefined}>
+                    <NavLink to="/skills" style={({isActive}) => isActive ? activeLink : link}>
                         Skills
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to="/projects" style={({isActive}) => isActive ? activeLink : undefined}>
+                    <NavLink to="/projects" style={({isActive}) => isActive ? activeLink : link}>
                         Projects
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to="/contact" style={({isActive}) => isActive ? activeLink : undefined}>
+                    <NavLink to="/contact" style={({isActive}) => isActive ? activeLink : link}>
                         Contact Me
                     </NavLink>
                 </li>
